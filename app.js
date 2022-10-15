985837cd9093be4bab82ab5116237104d04d9370
mjudianto <mathewjudianto0603@gmail.com>
2022-10-15 13:22:57 +0700

    mysql connection fixed

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const app = express();
 
-var mysql = require('mysql');
-// import { ConnectToDatabase } from './module/db-connection.js';
-const databaseModule = require('./module/db-connection.js');
+const dbConnection = require("./module/db-connection.js");
 
-const con = mysql.createConnection({
-  host: "127.0.0.1",
-  port: '3306',
-  user: "root",
-  password: "PkmOba_2022",
-  database: ""
+const con = dbConnection.connect();
+
+con.connect(function(err) {
+  if (err) throw err;
 });
 
-const db = databaseModule.Connect(con);
+dbConnection.dbQueryAll(con, 'kurikulum', function(result){
+  console.log(result);
+})
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- module/db-connection.js
+++ module/db-connection.js
@@ -1,12 +1,26 @@
-module.exports = {
-  Connect: function ConnectToDatabase(con){
-    con.connect(function(err) {
-      if (err) throw err;
-      con.query("SELECT * FROM OBA.kurikulum", function (err, result, fields) {
-        if (err) throw err;
-        // console.log(result[0].IDKurikulum);
-      });
-      console.log("Connected!");
-    });
-  }
+var mysql = require('mysql');
+
+function connect(){
+  const con = mysql.createConnection({
+    host: "127.0.0.1",
+    port: '3306',
+    user: "root",
+    password: "PkmOba_2022",
+    database: "OBA"
+  });
+
+  return con;
 }
+
+function dbQueryAll(con, tableName, cb){
+    var queryString = "SELECT * FROM " + tableName;
+    con.query(queryString, function (err, result) {
+    if (err) throw err;
+    
+    cb(result);
+  });
+}
+
+// function dbSeed(){}
+
+module.exports = {connect ,dbQueryAll};
